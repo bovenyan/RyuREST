fcc9eb5279269a20edb15078b342e78f7fd4fd68
bovenyan <bovenyan@gmail.com>
2015-06-19 16:40:09 -0400

    June 19 	A little more.

--- src/org/att/RyuAPI/OpenFlowMsg.java
+++ src/org/att/RyuAPI/OpenFlowMsg.java
@@ -1,45 +1,185 @@
 package org.att.RyuAPI;
 import java.util.*;
+import org.json.simple.*;
 
-public class OpenFlowMsg {	
+public class OpenFlowMsg {
 	String msg_type;
+	Map<String, String> msg = new LinkedHashMap<String, String>();
+	Map<String, String> match = new LinkedHashMap<String, String>();
+	LinkedList<LinkedHashMap<String, String>> actions = 
+			new LinkedList<LinkedHashMap<String, String>>();
+	ArrayList<Integer> buckets = new ArrayList<Integer>();
 	
-	// Setting
-	int dpid = 0;
-	int in_port = 0;
-	long cookie = 0;
-	long cookie_mask = 0xffffffffL;
-	int table_id = 0;
+	JSONObject msg_json = new JSONObject();
+	
+	public OpenFlowMsg(String type){
+		msg_type = type;
+	}
+	// Msg
+	public void set_dpid(int value){
+		msg.put("dpid", Integer.toString(value));
+	}
+	
+	public void set_in_port(int value){
+		msg.put("in_port", Integer.toString(value));
+	}
+	
+	public void set_cookie_mask(int cookie, long mask){
+		msg.put("cookie", Integer.toString(cookie));
+		msg.put("cookie", Long.toString(cookie));
+	}
+	
+	public void set_table_id(int value){
+		msg.put("table_id", Integer.toString(value));
+	}
+	
+	public void set_group_id(int value){
+		msg.put("group_id", Integer.toString(value));
+		
+		if (msg_type.equals("AddGroup"))
+			msg.put("type", "ALL");
+	}
+	
+	public void set_priority(int value){
+		msg.put("priority", Integer.toString(value));
+	}
 	
 	// Match
-	final int eth_type = 2048;
-	String ip_src = "";
-	String ip_src_mask = "";
-	String ip_dst = "";
-	String ip_dst_mask = "";
-	int nw_proto = 0;
-	int tcp_src = 0;
-	int tcp_dst = 0;
-	int udp_src = 0;
-	int udp_dst = 0;
-
-	// Flow Actions
-	int out_port = 0;
-	int out_group = 0;
-	String mod_ip_dst = "";
-	String mod_ip_src = "";
-	String mod_udp_dst = "";
-	String mod_udp_src = "";
+	public void set_ipv4_src(String value){
+		String [] parts = value.split("/");
+		
+		match.put("eth_type", "2048");
+		
+		if (parts.length == 1){
+			match.put("ip_src", value);
+		}
+		else{
+			String ip_src_mask = parts[0]+"/";
+			long mask_int = 0xffffffffL;
+			ip_src_mask += longToIpv4(mask_int << (32-Integer.parseInt(parts[1])));
+			match.put("ip_src_mask", ip_src_mask);
+		}
+	}
 	
-	// Group Actions
-	int group_id = 0;
-	List<Integer> out_ports = new ArrayList<Integer>();
+	public void set_ipv4_dst(String value){
+		String [] parts = value.split("/");
+		
+		match.put("eth_type", "2048");
+		
+		if (parts.length == 1){
+			match.put("ip_src", value);
+		}
+		else{
+			String ip_dst_mask = parts[0]+"/";
+			long mask_int = 0xffffffffL;
+			ip_dst_mask += longToIpv4(mask_int << (32-Integer.parseInt(parts[1])));
+			match.put("ip_dst_mask", ip_dst_mask);
+		}
+	}
 	
-	// Setting Map
-	Map<String, Boolean> setting = new HashMap<String, Boolean>();
+	public void set_tcp_src(int value){
+		match.put("eth_type", "2048");
+		match.put("nw_proto", "6");
+		match.put("tcp_src", Integer.toString(value));
+	}
 	
-	public OpenFlowMsg(String type) {
-		msg_type = type;
+	public void set_tcp_dst(int value){
+		match.put("nw_proto", "6");
+		match.put("tcp_dst", Integer.toString(value));
+	}
+	
+	public void set_udp_src(int value){
+		match.put("nw_proto", "17");
+		match.put("udp_src", Integer.toString(value));
+	}
+	
+	public void set_udp_dst(int value){
+		match.put("nw_proto", "17");
+		match.put("udp_dst", Integer.toString(value));
+	}
+	
+	// Actions
+	public void action_output(int value){
+		LinkedHashMap<String, String> output_action = 
+				new LinkedHashMap<String, String>();
+		output_action.put("type", "OUTPUT");
+		output_action.put("port", Integer.toString(value));
+		actions.add(output_action);
+	}
+	
+	public void action_to_controller(){
+		LinkedHashMap<String, String> output_action = 
+				new LinkedHashMap<String, String>();
+		output_action.put("type", "OUTPUT");
+		output_action.put("port", Long.toString(Long.parseLong("0xfffd",16)));
+		actions.add(output_action);
+	}
+	
+	public void action_to_l2l3(){
+		LinkedHashMap<String, String> output_action = 
+				new LinkedHashMap<String, String>();
+		output_action.put("type", "OUTPUT");
+		output_action.put("port", Long.toString(Long.parseLong("0xfffa",16)));
+		actions.add(output_action);
+	}
+	
+	public void action_set_ipv4_src(String srcIP){
+		LinkedHashMap<String, String> output_action = 
+				new LinkedHashMap<String, String>();
+		output_action.put("type", "SET_FIELD");
+		output_action.put("field", "ipv4_src");
+		output_action.put("value", srcIP);
+		actions.add(output_action);
+	}
+	
+	public void action_set_ipv4_dst(String dstIP){
+		LinkedHashMap<String, String> output_action = 
+				new LinkedHashMap<String, String>();
+		output_action.put("type", "SET_FIELD");
+		output_action.put("field", "ipv4_dst");
+		output_action.put("value", dstIP);
+		actions.add(output_action);
+	}
+	
+	public void action_set_tcp_src(String srcPort){
+		LinkedHashMap<String, String> output_action = 
+				new LinkedHashMap<String, String>();
+		output_action.put("type", "SET_FIELD");
+		output_action.put("field", "tcp_src");
+		output_action.put("value", srcPort);
+		actions.add(output_action);
+	}
+	
+	public void action_set_tcp_dst(String dstPort){
+		LinkedHashMap<String, String> output_action = 
+				new LinkedHashMap<String, String>();
+		output_action.put("type", "SET_FIELD");
+		output_action.put("field", "tcp_dst");
+		output_action.put("value", dstPort);
+		actions.add(output_action);
+	}
+	
+	public void action_set_udp_src(String srcPort){
+		LinkedHashMap<String, String> output_action = 
+				new LinkedHashMap<String, String>();
+		output_action.put("type", "SET_FIELD");
+		output_action.put("field", "udp_src");
+		output_action.put("value", srcPort);
+		actions.add(output_action);
+	}
+	
+	public void action_set_udp_dst(String dstPort){
+		LinkedHashMap<String, String> output_action = 
+				new LinkedHashMap<String, String>();
+		output_action.put("type", "SET_FIELD");
+		output_action.put("field", "udp_dst");
+		output_action.put("value", dstPort);
+		actions.add(output_action);
+	}
+	
+	// Buckets
+	public void bucket_outputs(ArrayList<Integer> out_ports){
+		buckets = new ArrayList<Integer>(out_ports);
 	}
 	
 	/**
@@ -58,219 +198,28 @@ public class OpenFlowMsg {
 		return IpStr.substring(0, IpStr.length()-1);
 	}
 	
-	/**
-	 * @param field the field that is set using (String, String) pair.
-	 * Including ip_src, ip_src. ip_src can be specified as 10.0.0.1, 
-	 * Or 10.0.0.0/24, which will set ip_src or ip_src_mask, respectively
-	 * @param value the value given to the corresponding field
-	 */
-	public void setField(String field, String value){
-		if (field.equals("ip_src")){
-			String [] parts = value.split("/");
-			
-			if (parts.length == 1){
-				ip_src = value;
-				setting.put("ip_src", true);
-			}
-			else{
-				ip_src_mask = parts[0]+"/";
-				long mask_int = 0xffffffffL;
-				ip_src_mask += longToIpv4(mask_int << (32-Integer.parseInt(parts[1])));
-				setting.put("ip_src_mask", true);
-			}
-		}
-		if (field.equals("ip_dst")){
-			String [] parts = value.split("/");
-			if (parts.length == 1){
-				ip_dst = value;
-				setting.put("ip_dst", true);
-			}
-			else{
-				ip_dst_mask = parts[0]+"/";
-				long mask_int = 0xffffffffL;
-				ip_dst_mask += longToIpv4(mask_int << (32-Integer.parseInt(parts[1])));
-				setting.put("ip_dst_mask", true);
-			}
-		}
-	}
-	
-	
-	public void setGroup(int gId, )
-	
-	/**
-	 * @param field the field that is set using (String, int) pair. 
-	 * Including: in_port, cookie, cookie_mask, nw_proto, tcp_src, 
-	 * tcp_dst, udp_src 
-	 * @param value the value given to the corresponding field.
-	 * e.g. setField("cookie_mask", 24) sets the cookie_mask to 0xffffff00 
-	 */
-	public void setField(String field, int value){
-		if (field.equals("in_port")){
-			in_port = value;
-			setting.put("in_port", true);
-			return;
-		}
-		if (field.equals("cookie")){
-			cookie = value;
-			setting.put("cookie", true);
-			return;
-		}
-		if (field.equals("cookie_mask")){
-			cookie_mask = 0xffffffffL & (0xffffffffL << (32-value));
-			setting.put("cookie_mask", true);
-			return;
-		}
-		if (field.equals("table_id")){
-			table_id = value;
-			setting.put("table_id", true);
-			return;
-		}
-		if (field.equals("tcp_src")){
-			nw_proto = 6;
-			tcp_src = value;
-			setting.put("tcp_src", true);
-			setting.put("nw_proto", true);
-			return;
-		}
-		if (field.equals("tcp_dst")){
-			nw_proto = 6;
-			tcp_dst = value;
-			setting.put("tcp_dst", true);
-			setting.put("nw_proto", true);
-			return;
-		}
-		if (field.equals("udp_src")){
-			nw_proto = 17;
-			udp_src = value;
-			setting.put("udp_src", true);
-			setting.put("nw_proto", true);
-			return;
-		}
-		if (field.equals("udp_dst")){
-			nw_proto = 17;
-			udp_dst = value;
-			setting.put("udp_dst", true);
-			setting.put("nw_proto", true);
-			return;
-		}
-	}
-	
-	
-	
-	public String toREST(){
-		StringBuilder sb = new StringBuilder();
-		sb.append("{");
-		if (setting.get("dpid")){
-			sb.append("\"dpid:\"");
-			sb.append(dpid);
-			sb.append(",");
-		}
-		else{ // missing dpid
-			return "";
-		}
+	public String toJson(){
+		msg_json = new JSONObject(msg);
 		
-		if (setting.get("cookie")){
-			sb.append("\"cookie\":");
-			sb.append(cookie);
-			sb.append(",");
-		}
-		if (setting.get("cookie_mask")){
-			sb.append("\"cookie_mask\": ");
-			sb.append(cookie_mask);
-			sb.append(",");
-		}
-		if (setting.get("table_id")){
-			sb.append("\"table_id\": ");
-			sb.append(table_id);
-			sb.append(",");
-		}
-		
-		// build match
 		if (msg_type.contains("Flow")){
-			sb.append("\"match\":{");
-			
-			if (setting.get("in_port")){
-				sb.append("\"in_port\":");
-				sb.append(in_port);
-				sb.append(",");
-			}
-			
-			if (setting.get("ip_src")){
-				sb.append("\"ip_src\":");
-				sb.append(ip_src);
-				sb.append(",");
-			}
-			
-			if (setting.get("ip_dst")){
-				sb.append("\"ip_dst\":");
-				sb.append(ip_dst);
-				sb.append(",");
-			}
-			
-			if (setting.get("tcp_src")){
-				sb.append("\"tcp_src\":");
-				sb.append(tcp_src);
-				sb.append(",");
-			}
-			
-			if (setting.get("tcp_dst")){
-				sb.append("\"tcp_dst\":");
-				sb.append(tcp_dst);
-				sb.append(",");
-			}
-			
-			if (setting.get("udp_src")){
-				sb.append("\"udp_src\":");
-				sb.append(udp_src);
-				sb.append(",");
-			}
-			
-			if (setting.get("udp_dst")){
-				sb.append("\"udp_dst\":");
-				sb.append(udp_dst);
-				sb.append(",");
-			}
-			
-			if (sb.length() > 0){ // delete last comma
-				sb.setLength(sb.length()-1);
-			}
-			
-			sb.append("},");
+			msg_json.put("match", match);
 			
 			if (msg_type.equals("AddFlow")){
-				sb.append("\"actions\":[");
-				if (setting.get("to_controller")){
-					
-				}
-				sb.append("]");
+				msg_json.put("actions", actions);
 			}
+			
 		}
-		
-		if (msg_type.contains("Group")){
-			if (setting.get("group_id")){
-				
-				sb.append("\"group_id\":");
-				sb.append(group_id);
-				sb.append(",");
-				
-				if (msg_type.equals("AddGroup")){
-					sb.append("\"type\": \"ALL\",");
-					
-					sb.append("\"buckets\": [");
-				}
-			}
-			else{
-				return "";
-			}
+		else if (msg_type.contains("Group")){
+			JSONObject bucket_json = new JSONObject();
+			
+			
+			
+			msg_json.put("buckets", bucket_json);
+			
+			
+			
 		}
-		
-		
-		
-		
-		
-		sb.append("}");
-		
-		return sb.toString();
+		return msg_json.toString();
 	}
 
 }
